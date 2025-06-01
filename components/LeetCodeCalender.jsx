597711d6d9c76ce3092975137bae74381885eb80
DivyGandhi <divygandhi4978@gmail.com>
2025-06-01 18:50:40 +0530

    day-1 submission a

--- components/LeetCodeCalender.jsx
+++ components/LeetCodeCalender.jsx
@@ -1,23 +1,11 @@
-'use client';
+import dynamic from "next/dynamic";
 
-import React from "react";
-import LeetCodeCalendar from "leetcode-calendar";
+const LeetCodeCalendarComponent = dynamic(() => import("../components/LeetCodeCalendarComponent"), { ssr: false });
 
-export default function LeetCodeCalendarComponent() {
-  // Custom theme with GitHub-like green colors, from light to dark
-  const customTheme = {
-    dark: ["#161b22", "#9be9a8", "#40c463", "#30a14e", "#216e39"]
-  };
-
+export default function Home() {
   return (
-    <div >
-      <LeetCodeCalendar
-        username="divygandhi4978"
-        blockSize={15}     
-        blockMargin={3}  
-        fontSize={14}      
-        theme={customTheme}  
-      />
+    <div>
+      <LeetCodeCalendarComponent />
     </div>
   );
 }
