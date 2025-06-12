6ff17ca7fe99c8e09030f3624867e7acfca9f7c3
DivyGandhi <divygandhi4978@gmail.com>
2025-06-12 13:43:17 +0530

    Final commit of personal portfolio project

--- components/Conclusion.jsx
+++ components/Conclusion.jsx
@@ -4,10 +4,10 @@ import profile from "../constants/images/profile1.jpg";
 const Conclusion = () => {
   return (
     <div className="max-w-4xl">
-      <h1 className="text-center text-4xl font-medium mt-20 ">About Me</h1>
+      <h1 className="text-center text-4xl font-medium mt-20 mb-10">About Me</h1>
 
       <div className="sm:flex justify-between items-center">
-        <div className="text-xl sm:max-w-3/5 font">
+        <div className="text-xl sm:max-w-3/5 ">
           Hi, I’m <span className="text-[#956c7a] font-bold"> Divy Gandhi</span>
           , a passionate Web Developer with a keen eye for creating seamless and
           engaging digital experiences. With a strong foundation in front-end
