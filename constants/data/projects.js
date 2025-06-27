501e16fca22147594b3acaca4fe03aa1a7317d65
DivyGandhi <divygandhi4978@gmail.com>
2025-06-27 16:43:50 +0530

    mail send

--- constants/data/projects.js
+++ constants/data/projects.js
@@ -5,20 +5,12 @@ import newsletter from "../images/newsletter.png";
 
 const list = [
   {
-    head: "SecureHold",
-    data: "Synchronize your passwords across multiple devices and explore additional security features.",
-    preview: "https://secure-hold-1.onrender.com/",
-    source: "https://github.com/divygandhi4978/secure-hold",
-    img: securehold,
-    skills:["MERN","JWT"]
-  },
-  {
-    head: "URLShortner",
-    data: "Shorten your urls with custom alias and expiration with QR Code.",
-    preview: "https://urlshortner1-0gcr.onrender.com/",
-    source: "https://github.com/divygandhi4978/urlShortner",
-    img: urlshortner,
-    skills:["ReactJs",'NodeJs',"Express"]
+    head: "Money Moves",
+    data: "Articles displayed",
+    preview: "https://moneymoves1.netlify.app/",
+    source: "https://github.com/divygandhi4978/personal-pf",
+    img: newsletter,
+    skills: ["MERN", "API use for Frontend"],
   },
   {
     head: "Personal portfolio",
@@ -26,15 +18,23 @@ const list = [
     preview: "https://personal-pf-alpha.vercel.app/",
     source: "https://github.com/divygandhi4978/personal-pf",
     img: portfolio,
-    skills:["NextJs","NodeMailer"]
+    skills: ["NextJs", "NodeMailer"],
   },
   {
-    head: "Money Moves",
-    data: "Articles displayed",
-    preview: "https://moneymoves1.netlify.app/",
-    source: "https://github.com/divygandhi4978/personal-pf",
-    img: newsletter,
-    skills:["NextJs","NodeMailer"]
+    head: "SecureHold",
+    data: "Synchronize your passwords across multiple devices and explore additional security features.",
+    preview: "https://secure-hold-1.onrender.com/",
+    source: "https://github.com/divygandhi4978/secure-hold",
+    img: securehold,
+    skills: ["MERN", "JWT"],
+  },
+  {
+    head: "URLShortner",
+    data: "Shorten your urls with custom alias and expiration with QR Code.",
+    preview: "https://urlshortner1-0gcr.onrender.com/",
+    source: "https://github.com/divygandhi4978/urlShortner",
+    img: urlshortner,
+    skills: ["ReactJs", "NodeJs", "Express"],
   },
 ];
 
